a1ca9af6013bdec0df11ee9e93fed36f8bbb4602
sunyutong <18147508318@163.com>
2020-06-28 14:36:59 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import liebiaoye from '@/pages/memeda/liebiaoye'
+import xiangqingye from '@/pages/memeda/xiangqingye'
 
 Vue.use(Router)
 
@@ -10,6 +12,22 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/liebiaoye',
+      name: 'liebiaoye',
+      component: liebiaoye,
+      meta:{
+        title:'列表'
+      }
+    },
+    {
+      path: '/xiangqingye',
+      name: 'xiangqingye',
+      component: xiangqingye,
+      meta:{
+        title:'详情'
+      }
     }
   ]
 })
